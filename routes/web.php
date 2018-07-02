652864d14ee94b3d9a4efaf3186e67cbf1a930c2
pan1829@163.com <=>
2018-07-02 10:34:17 +0000

    Finish layout and routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home'); //主页
-Route::get('/help', 'StaticPagesController@help');//帮助
-Route::get('/about', 'StaticPagesController@about');//关于
+Route::get('/', 'StaticPagesController@home')->name('home'); //主页
+Route::get('/help', 'StaticPagesController@help')->name('help');//帮助
+Route::get('/about', 'StaticPagesController@about')->name('about');//关于
+
+Route::get('signup', 'UsersController@create')->name('signup');//注册
